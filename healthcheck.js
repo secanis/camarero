5a7d6bff087c5766fe3077f6fbeedd84518017d6
Matthias Baldi <matthias.baldi@secanis.ch>
2018-12-27 16:12:16 +0100

    fix: improve healthcheck

--- healthcheck.js
+++ healthcheck.js
@@ -1,15 +1,15 @@
-const http = require("http");
-const conf = require('./conf/default.conf');
+const config = require(`${__dirname}/conf/default.conf.js`);
+const http = require('http');
 
 const options = {
-    host: "localhost",
-    port: conf.express.port,
+    host: 'localhost',
+    port: config.express.port || 3000,
     timeout: 2000
 };
 
 let request = http.request(options, (res) => {
-    console.info(`STATUS: ${res.statusCode}`);
-    if (res.statusCode == 200) {
+    console.info(`STATUS: ${res.statusMessage}`);
+    if (res.statusCode < 400) {
         process.exit(0);
     } else {
         process.exit(1);
@@ -17,8 +17,9 @@ let request = http.request(options, (res) => {
 });
 
 request.on('error', (err) => {
+    console.error(err);
     console.error('ERROR: unexpected error, please check the logs');
     process.exit(1);
 });
 
-request.end();
+request.end();  
